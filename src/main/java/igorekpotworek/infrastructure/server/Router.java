09713d0ed6f6efdb95f894570ad676eb8bf94463
igorekpotworek <igipopko@gmail.com>
2020-09-15 00:25:57 +0200

    minor adjustments

--- src/main/java/igorekpotworek/infrastructure/server/Router.java
+++ src/main/java/igorekpotworek/infrastructure/server/Router.java
@@ -3,6 +3,7 @@ package igorekpotworek.infrastructure.server;
 import igorekpotworek.infrastructure.server.error.ExceptionHandler;
 import igorekpotworek.infrastructure.server.error.RouteNoteFoundException;
 import igorekpotworek.infrastructure.server.lifecycle.CloseConnectionPredicate;
+import io.vavr.API;
 import io.vavr.collection.List;
 import io.vavr.collection.Stream;
 import io.vavr.control.Option;
@@ -13,8 +14,8 @@ import java.io.BufferedReader;
 import java.io.PrintWriter;
 import java.net.SocketTimeoutException;
 
-import static io.vavr.API.Option;
 import static io.vavr.Predicates.not;
+import static io.vavr.control.Option.none;
 import static java.util.function.Function.identity;
 
 @RequiredArgsConstructor
@@ -35,14 +36,17 @@ class Router {
   }
 
   private Option<String> tryRead(BufferedReader in) {
-    return Option(Try.of(in::readLine).recover(SocketTimeoutException.class, (__) -> null).get());
+    return Try.of(in::readLine)
+        .map(API::Option)
+        .recover(SocketTimeoutException.class, (__) -> none())
+        .get();
   }
 
   private Response handleRequest(Request request, Session session) {
     return controllers
         .find(it -> it.matches(request.getBody()))
         .toTry(RouteNoteFoundException::new)
-        .flatMap(it -> it.handle(new Request(request.getBody()), session))
+        .flatMap(it -> it.handle(request, session))
         .getOrElseGet(exceptionHandler::handle);
   }
 }
